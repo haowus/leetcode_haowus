2b1237a43aea604e01b43eb56ae307f393888677
hao_wu <1411981538@qq.com>
2019-12-19 12:19:26 +0800

    origin data

---
+++ src/algorithms/add_two_numbers.java
@@ -0,0 +1,21 @@
+package algorithms;
+
+
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int x){val = x;}
+}
+public class add_two_numbers {
+    public ListNode addTwoNumbers(ListNode l1,ListNode l2){
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next = new ListNode(3);
+        System.out.println();
+    }
+}
