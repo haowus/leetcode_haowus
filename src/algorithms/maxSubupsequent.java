9543d6fbcd7e2634d4078802bffd51deb77db7c7
haowus <wuhao19950919>
2020-02-11 22:17:57 +0800

    3

---
+++ src/algorithms/maxSubupsequent.java
@@ -0,0 +1,21 @@
+package algorithms;
+
+public class maxSubupsequent {
+    public int findLengthOfLCIS(int[] nums) {
+
+        int max=nums[0],count=1,maxCount=0;
+        for (int i=1;i<nums.length;i++){
+            if (nums[i]>max){
+                max = nums[i];
+                count++;
+                if (count>maxCount)
+                    maxCount=count;
+            }
+            else{
+                max=nums[i];
+                count=1;
+            }
+        }
+        return maxCount;
+    }
+}
--- src/algorithms/testAll.java
+++ src/algorithms/testAll.java
@@ -6,6 +6,10 @@ public class testAll {
 //        int[] nums = new int[]{1,12,-5,-6,50,3};
 //        int k =4;
 //        System.out.println(maxAverage.findMaxAverage(nums,k));
-        System.out.println(Math.round(51/4));
+//        System.out.println(Math.round(51/4));
+//        maxSubupsequent maxSubupsequent = new maxSubupsequent();
+        du_array du_array = new du_array();
+        int [] nums = new int[]{1,2,3,2,5};
+        System.out.println(du_array.findShortestSubArray(nums));
     }
 }
