88c99771b499341accb4bb2537368f3047796d1e
haowus <wuhao19950919>
2020-02-13 22:48:14 +0800

    9

---
+++ src/algorithms/monotonic.java
@@ -0,0 +1,17 @@
+package algorithms;
+
+public class monotonic {
+    public boolean isMonotonic(int[] A) {
+        int countUP=0,countDOWN=0,count0=0;
+        for(int i=0;i<A.length-1;i++){
+            if(A[i+1]<A[i])
+                countDOWN++;
+            else if (A[i+1]>A[i])
+                countUP++;
+            else
+                count0++;
+        }
+
+        return (count0+countUP==A.length-1||count0+countDOWN==A.length-1) ?true:false;
+    }
+}
--- src/algorithms/testAll.java
+++ src/algorithms/testAll.java
@@ -11,8 +11,16 @@ public class testAll {
 //        du_array du_array = new du_array();
 //        int [] nums = new int[]{1,2,3,2,5};
 //        System.out.println(du_array.findShortestSubArray(nums));
-        largeGroup l = new largeGroup();
-        String S = "aaa";
-        System.out.println(l.largeGroupPositions(S));
+//        largeGroup l = new largeGroup();
+//        String S = "aaa";
+//        System.out.println(l.largeGroupPositions(S));
+//        maxDist maxDist = new maxDist();
+//        int [] nums = new int[]{1,0,0,1,0,0,0,1,1};
+//        System.out.println(maxDist.maxDistToClosest(nums));
+        sortbyParity sortbyParity = new sortbyParity();
+        int[] nums = new int[]{3,1,2,4};
+        System.out.println(sortbyParity.sortArrayByParity(nums));
+
+
     }
 }
