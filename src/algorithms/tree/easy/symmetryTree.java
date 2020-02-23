59e6bcd74d16c36f3ce1af887f85dc3507a977db
haowus <wuhao19950919>
2020-02-23 17:02:17 +0800

    0

---
+++ src/algorithms/tree/easy/symmetryTree.java
@@ -0,0 +1,15 @@
+package algorithms.tree.easy;
+
+public class symmetryTree {
+    public class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode (int x) {val = x;}
+    }
+
+    public boolean isSymmetric(TreeNode root){
+
+        return true;
+    }
+}
