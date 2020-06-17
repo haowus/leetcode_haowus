52a0579f09defb1655541fd3eca82a71b67a3e87
hao_wu <1411981538@qq.com>
2020-06-17 20:56:52 +0800

    1

---
+++ src/leetcode_sf_offer/ListNode.java
@@ -0,0 +1,13 @@
+package leetcode_sf_offer;
+
+
+
+/**
+ * @Author haowus919@gmail.com in lab
+ * @Date 2020/6/17 20:34
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x){val = x;}
+}
