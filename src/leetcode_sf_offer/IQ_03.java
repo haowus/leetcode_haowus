eb3536d0a84154561799e51a9f5cbb9bf142fe5c
haowus <wuhao19950919>
2020-07-20 10:44:27 +0800

    1

--- src/leetcode_sf_offer/IQ_03.java
+++ src/leetcode_sf_offer/IQ_03.java
@@ -8,13 +8,35 @@ package leetcode_sf_offer;
 public class IQ_03 {
     public int findRepeatNumber(int[] nums) {
 
-        int[] res = new int[nums.length];
-        for (int i: nums){        //用相同大小的数组存储原数组中的元素，
-            res[i]++;             //当发现新数组里的值>1，return。 即找到
-            if (res[i]>1)
-                return i;
-        }
-        return -1;
+//        int[] res = new int[nums.length];
+//        for (int i: nums){        //用相同大小的数组存储原数组中的元素，
+//            res[i]++;             //当发现新数组里的值>1，return。 即找到
+//            if (res[i]>1)
+//                return i;
+//        }
+//        return -1;
+        int res = 0;
+        for (int i=0;i<nums.length;i++){
 
+            while (i!=nums[i]){
+                if (nums[i]==nums[nums[i]]){
+                    res = nums[i];
+                    break;
+                }
+                swap(i,nums[i],nums);
+            }
+        }
+        return res;
+    }
+
+    private void swap(int i,int j,int[] nums){
+        int temp = nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+
+    public static void main(String[] args) {
+        IQ_03 iq_03 = new IQ_03();
+        System.out.println(iq_03.findRepeatNumber(new int[]{2, 3, 1, 0, 2, 5, 3}));
     }
 }
